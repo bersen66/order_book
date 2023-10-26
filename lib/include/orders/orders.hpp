ce99fe406817a8e931cbe9d2e6d12bb1ea9ecc7f
ortur <orturb@yandex.ru>
2023-10-26 17:04:08 +0300

    Feat(book): Added interface IOrderBook

--- lib/include/orders/orders.hpp
+++ lib/include/orders/orders.hpp
@@ -1,4 +1,8 @@
 #pragma once
 
 #include <orders/order.hpp>
-#include <orders/order_book.hpp>
+#include <orders/iorder_book.hpp>
+#include <orders/unlimited_order_book.hpp>
+#include <orders/limited_order_book.hpp>
+
+using OrderBook = UnlimitedOrderBook;
